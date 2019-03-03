ba3fecb96ca36e211021d5ec2822979126972b36
davemullenjnr <davemullenjnr@gmail.com>
2019-03-03 21:28:20 +0000

    add caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
