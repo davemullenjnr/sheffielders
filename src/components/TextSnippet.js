9c5b3f6ef8d3762d58da23358e834ed509d48076
davemullenjnr <davemullenjnr@gmail.com>
2019-03-10 22:54:37 +0000

    Update TextSnippet.js

--- src/components/TextSnippet.js
+++ src/components/TextSnippet.js
@@ -4,26 +4,24 @@ import styled from 'styled-components'
 import { typography, variables } from '../theme';
 
 const Title = styled.h4`
-  font-family: ${typography.fontSans};
-  font-weight: normal;
-  text-align: center;
-  text-transform: uppercase;
-  letter-spacing: ${variables.spacer / 2}rem;
-  margin-bottom: ${variables.spacer}rem;
+  font-size: ${typography.f5};
+  color: ${variables.black};
+  font-weight: bold;
+  margin-bottom: ${variables.spacer * 2}rem;
 `
 
 const Box = styled.div`
-  border: 1px solid ${variables.black};
-  border-radius: ${variables.spacer}rem;
+  border-left: 4px solid ${variables.brand};
+  border-radius: ${variables.spacer / 2}rem;
   padding: ${variables.spacer * 3}rem;
+  background-color: white;
+  box-shadow: 0px 4px 16px 4px rgba(0,0,0,0.1);
 `
 
 const Description = styled.p`
   font-family: ${typography.fontSerif};
-  font-weight: bold;
-  line-height: 2;
-  font-size: 14px;
-  text-align: center;
+  line-height: 1.8;
+  font-size: ${typography.f6};
 `
 
 class TextSnippet extends Component {
