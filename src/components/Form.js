203263d43e0087aa4232a30de5c079baf4aa1a48
Dave Mullen Jnr <davemullenjnr@gmail.com>
2025-09-18 22:35:02 +0100

    updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-const WufooForm = require('react-wufoo-embed');
-
 const Form = () => {
-  
   return (
     <div>
-      <WufooForm userName="hoodiehut1" formHash="z1qm9qyi0efacmf" header="hide" />
+      <iframe
+        height="480"
+        allowTransparency="true"
+        frameBorder="0"
+        scrolling="no"
+        style={{ width: '100%', border: 'none' }}
+        src="https://hoodiehut1.wufoo.com/embed/z1qm9qyi0efacmf/def/embedKey=z1qm9qyi0efacmf17729"
+      >
+        <a
+          href="https://hoodiehut1.wufoo.com/forms/z1qm9qyi0efacmf/"
+          title="Fill out my Wufoo form!"
+        >
+          Fill out my Wufoo form!
+        </a>
+      </iframe>
     </div>
   )
 }
